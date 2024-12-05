e12f6d073d3451aafc52259aa5ec381f9f7b9e39
RmStark28 <Robert.Stark@Tufts.edu>
2024-12-05 16:33:58 -0500

    New

--- index.js
+++ index.js
@@ -1,28 +1,100 @@
-const { MongoClient } = require('mongodb');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const querystring = require("querystring");
 
 const uri = "mongodb+srv://robertstark:123@cluster0.ynfto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+const db = "Stock";
+const collection = "PublicCompanies";
 
-async function main() {
-    try {
-        // Use connect method to connect to the server
-        await client.connect();
-        console.log('Connected successfully to server');
+const port = process.env.PORT || 3000;
 
-        // Get the admin database
-        const adminDb = client.db().admin();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-        // List databases
-        const databasesList = await adminDb.listDatabases();
+async function searchCompanies(query, type) {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        console.log('Databases:');
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-    } catch (error) {
-        console.error('An error occurred:', error);
-    } finally {
-        // Close connection
-        await client.close();
-    }
+  //   // Select the db and collection
+  //   const database = client.db(db);
+  //   const companies = database.collection(collection);
+
+  //   // Search for companies
+  //   if (type === "ticker") {
+  //     return await companies.find({ Ticker: query }).toArray();
+  //   } else if (type === "company") {
+  //     return await companies.find({ Company: query }).toArray();
+  //   }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching data from MongoDB");
+  } finally {
+    await client.close();
+  }
+  // Return a garbage value for testing
+  // return [{ Company: "TestCompany", Ticker: "TEST", Price: 0 }];
 }
 
-main().catch(console.error);
+http.createServer(async function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var urlObj = url.parse(req.url, true);
+    var path = urlObj.pathname;
+
+    // If the path is /, read the home.html file
+    if (path == "/") {
+        fs.readFile("views/home.html", function (err, data) {
+            if (err) {
+                res.write("Error reading html");
+                return res.end();
+            } else {
+                res.write(data);
+                return res.end();
+            }
+        });
+    }
+    // If the path is /process, process the search query
+    else if (path == "/process") {
+        const query = urlObj.query.query;
+        const type = urlObj.query.type;
+
+        try {
+            const companies = await searchCompanies(query, type);
+
+            // Log info in console for debugging
+            companies.forEach((company) => {
+                console.log(`Name: ${company.Company}, Ticker: ${company.Ticker}, Price: $${company.Price}`);
+            });
+
+            // Create the HTML content
+            let companyHtml = companies.map((company) => {
+                return `<div>
+                          <h3>${company.Company}</h3>
+                          <p>Ticker: ${company.Ticker}</p>
+                          <p>Price: $${company.Price}</p>
+                        </div>`;
+            }).join('');
+
+            // Read and add data to process.html
+            const data = fs.readFileSync("views/process.html", "utf8");
+            const htmlContent = data.replace("<!-- place data here -->", companyHtml);
+            res.write(htmlContent);
+            return res.end();
+
+        } catch (error) {
+            console.error(error);
+            return res.end();
+        }
+    }
+}).listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
