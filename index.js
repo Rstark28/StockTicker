3658bd3c4bbd6345a62bb982da14f514e16041b4
RmStark28 <Robert.Stark@Tufts.edu>
2024-12-05 15:59:34 -0500

    New

--- index.js
+++ index.js
@@ -1,29 +1,74 @@
-const http = require('http');
-const { MongoClient } = require('mongodb');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const querystring = require("querystring");
 
-const uri = "mongodb+srv://robertstark:123@cluster0.ynfto.mongodb.net/?retryWrites=true&w=majority&ssl=true";
+const uri = "mongodb+srv://robertstark:123@cluster0.ynfto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const db = "Stock";
+const collection = "PublicCompanies";
+
 const port = process.env.PORT || 3000;
 
-async function testMongoConnection() {
-  try {
-    const client = new MongoClient(uri);
-    await client.connect();
-    console.log("Connected to MongoDB!");
-    await client.close();
-    return "MongoDB connection successful!";
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
-    return `MongoDB connection failed: ${error.message}`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
+});
+
+async function searchCompanies(query, type) {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  //   // Select the db and collection
+  //   const database = client.db(db);
+  //   const companies = database.collection(collection);
+
+  //   // Search for companies
+  //   if (type === "ticker") {
+  //     return await companies.find({ Ticker: query }).toArray();
+  //   } else if (type === "company") {
+  //     return await companies.find({ Company: query }).toArray();
+  //   }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching data from MongoDB");
+  } finally {
+    await client.close();
+  }
+  // Return a garbage value for testing
+  // return [{ Company: "TestCompany", Ticker: "TEST", Price: 0 }];
 }
 
-http.createServer(async (req, res) => {
-  if (req.url === "/test-db") {
-    const message = await testMongoConnection();
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(message);
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
-}).listen(port, () => console.log(`Server running on port ${port}`));
+http.createServer(async function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var urlObj = url.parse(req.url, true);
+    var path = urlObj.pathname;
+
+    // If the path is /, read the home.html file
+    if (path == "/") {
+        fs.readFile("views/home.html", function (err, data) {
+            if (err) {
+                res.write("Error reading html");
+                return res.end();
+            } else {
+                res.write(data);
+                return res.end();
+            }
+        });
+    }
+    // If the path is /process, process the search query
+    else if (path == "/process") {
+        const query = urlObj.query.query;
+        const type = urlObj.query.type;
+        const companies = await searchCompanies(query, type);
+        return res.end();
+    }
+}).listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
